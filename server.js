66005de5d75475e432b10c1d8c4a1d745f92c35b
namachand <49386827+namachand@users.noreply.github.com>
2024-01-11 17:30:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,37 +8,33 @@
 
 
 
-// app.get('/webhook', (req, res) => {
+app.get('/webhook', (req, res) => {
 
-//     const VERIFY_TOKEN = "random string"
-//     // Parse the query params
-//     console.log(req.query);
-//     const mode = req.query['hub.mode'];
-//     const token = req.query['hub.verify_token'];
-//     const challenge = req.query['hub.challenge'];
+    const VERIFY_TOKEN = "random string"
+    // Parse the query params
+    console.log(req.query);
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
 
-//     // Checks if a token and mode is in the query string of the request
-//     if (mode && token) {
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
 
-//       // Checks the mode and token sent is correct
-//       if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-//         // Responds with the challenge token from the request
-//         console.log('WEBHOOK_VERIFIED');
-//         res.status(200).send(challenge);
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
 
-//       } else {
-//         // Responds with '403 Forbidden' if verify tokens do not match
-//         res.sendStatus(403);      
-//       }
-//     }
-// })
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+})
 
-
-
-app.get("/webhook",(req,res)=>{
-  res.send("hello world")
-})  
+ 
 
 
 app.post('/webhook', (req, res) => {
