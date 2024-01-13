3fe7d2805160d5d51358f8d6f2e69f0c42cba1f4
namachand <49386827+namachand@users.noreply.github.com>
2024-01-13 17:23:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,54 +1,48 @@
-   const app = require('express')()
+const express = require('express');
+const app = express();
+const path = require("path");
 
-   // express middleware
-   const bodyParser = require('body-parser')
-   app.use(bodyParser.urlencoded({ extended: false }))
-   app.use(bodyParser.json())
+// express middleware
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/home", async (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "random string";
+  // Parse the query params
+  console.log(req.query);
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
 
-app.get('/webhook', (req, res) => {
-
-    const VERIFY_TOKEN = "random string"
-    // Parse the query params
-    console.log(req.query);
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
-})
+  }
+});
 
- 
+app.post("/webhook", (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-
-app.post('/webhook', (req, res) => {
-	try {
-		console.log(req.body);
-	}
-	catch(error) {
-		console.error(error);
-	}
-})
-
-   const PORT = 5000
-   // start server  
-   app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
-
-
-
+const PORT = 5000;
+// start server
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
